38b0275b4be6cc5a0850259ae1b5215d30eb4ff6
baldrick615 <tmcgrath28@gmail.com>
2025-01-20 09:48:19 -0600

    Add project files.

---
+++ LoopEx/Program.cs
@@ -0,0 +1,55 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        string newfees = "input.csv";
+        string fileName = "output.txt";
+        string path = "C:\\Users\\tmcgr\\OneDrive\\Desktop\\";
+        string filepath = path + newfees;
+        var array1 = ReadCsvFile(path + newfees);
+
+        //Facility codes
+        var array2 = new string[] { "18", "19", "21", "22", "23" };
+
+        //Delete the file if it exists
+        if (!File.Exists(path + fileName))
+        {
+            File.Create(path + fileName).Close();
+        }
+        else
+        {
+            File.Delete(path + fileName);
+            File.Create(path + fileName).Close();
+        }
+
+        //Loop through values and create the output file
+        for (int i = 0; i < array1.Length; i++)
+        {
+            if (array1[i][3] == "FACILITY")
+            {
+                for (int j = 0; j < array2.Length; j++)
+                {
+                    Console.WriteLine(array1[i][0] + ", " + array1[i][1] + ", " + array1[i][2] + ", " + array2[j]);
+                    //output the values to a csv file
+                    using (var sw = new StreamWriter(path + fileName, true))
+                    {
+                        sw.WriteLine(array1[i][0] + ", " + array1[i][1] + ", " + array1[i][2] + ", " + array2[j]);
+                    }
+                }
+            }
+            else
+            {
+                //output the values to a csv file
+                using (var sw = new StreamWriter(path + fileName, true))
+                {
+                    sw.WriteLine(array1[i][0] + ", " + array1[i][1] + ", " + array1[i][2] + "");
+                }
+            }
+        }
+    }
+
+    static string[][] ReadCsvFile(string filePath) { 
+        var lines = File.ReadAllLines(filePath); 
+        return lines.Select(line => line.Split(',')).ToArray(); 
+    }
+}
